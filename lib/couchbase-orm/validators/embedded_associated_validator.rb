c0bf0bbb1bef73f584f1390e9fe355326a37b2e1
Giallombardo Nathan <nathan.giallombardo@melting-point.fr>
2025-04-03 16:53:29 +0200

    fix validation

--- lib/couchbase-orm/validators/embedded_associated_validator.rb
+++ lib/couchbase-orm/validators/embedded_associated_validator.rb
@@ -8,16 +8,32 @@ module CouchbaseOrm
           next if v.valid?
 
           record.errors.add(attribute, "item ##{i} is invalid")
-          v.errors.each do |k, msg|
-            record.errors.add("#{attribute}_#{i}_#{k}".to_sym, msg)
+          v.errors.each do |*args|
+            if args.first.respond_to?(:attribute) # rails 7
+              error = args.first
+              key = "#{attribute}_#{i}_#{error.attribute}".to_sym
+              msg = error.message
+            else # Rails 5/6
+              k, msg = args
+              key = "#{attribute}_#{i}_#{k}".to_sym
+            end
+            record.errors.add(key, msg)
           end
         end
       else
         return if value.nil? || value.valid?
 
         record.errors.add(attribute, 'is invalid')
-        value.errors.each do |k, msg|
-          record.errors.add("#{attribute}_#{k}".to_sym, msg)
+        value.errors.each do |*args|
+          if args.first.respond_to?(:attribute) # rails 7
+            error = args.first
+            key = "#{attribute}_#{error.attribute}".to_sym
+            msg = error.message
+          else # Rails 5/6
+            k, msg = args
+            key = "#{attribute}_#{k}".to_sym
+          end
+          record.errors.add(key, msg)
         end
       end
     end
