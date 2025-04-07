16415e0eaec81e0ef3ea5300ca7c03017450b4d9
Giallombardo Nathan <nathan.giallombardo@melting-point.fr>
2025-04-07 16:10:40 +0200

    implement lazy loading attributes

--- lib/couchbase-orm/persistence.rb
+++ lib/couchbase-orm/persistence.rb
@@ -4,6 +4,7 @@
 require 'active_model'
 require 'active_support/hash_with_indifferent_access'
 
+# rubocop:disable Metrics/ModuleLength
 module CouchbaseOrm
   module Persistence
     extend ActiveSupport::Concern
@@ -13,6 +14,8 @@ module CouchbaseOrm
     included do
       attribute :id, :string
     end
+
+    Metadata = Struct.new(:cas)
 
     module ClassMethods
       def create(attributes = nil, &block)
@@ -35,45 +38,90 @@ module CouchbaseOrm
         end
       end
 
-        # Raise an error if validation failed.
+      # Raise an error if validation failed.
       def fail_validate!(document)
         raise Error::RecordInvalid.new(document)
       end
 
-        # Allow classes to overwrite the default document name
-        # extend ActiveModel::Naming (included by ActiveModel::Model)
+      # Allow classes to overwrite the default document name
+      # extend ActiveModel::Naming (included by ActiveModel::Model)
       def design_document(name = nil)
         return @design_document unless name
 
         @design_document = name.to_s
       end
 
-        # Set a default design document
+      # Set a default design document
       def inherited(child)
         super
         child.instance_eval do
           @design_document = child.name.underscore
         end
       end
+
+      def instantiate(attributes, id, cas, klass)
+        # attributes = decode_encrypted_attributes(attributes)
+        type = attributes.delete('type')
+        if type && type.to_s != klass.design_document
+          raise CouchbaseOrm::Error::TypeMismatchError.new(
+            "document type mismatch, #{type} != #{klass.design_document}", self
+          )
+        end
+
+        # Use ActiveModel::AttributeSet::Builder to build attributes
+        builder = attributes_builder
+        attribute_set = builder.build_from_database(attributes)
+
+        # Allocate and initialize the object
+        instance = klass.allocate
+        instance.init_with('attributes' => attribute_set, 'id' => id, 'cas' => cas, 'new_record' => false) # Custom initialization method
+        instance
+      end
     end
 
-      # Returns true if this object hasn't been saved yet -- that is, a record
-      # for the object doesn't exist in the database yet; otherwise, returns false.
+    def init_with(coder)
+      CouchbaseOrm.logger.debug { "Initialize model #{model} with #{coder['attributes'].to_s.truncate(200)}" }
+      @__metadata__ = Metadata.new
+      @attributes = coder['attributes']
+
+      init_internals
+
+      @new_record = coder['new_record']
+
+      unless @new_record
+        @attributes.write_from_database('id', coder['id'])
+        @__metadata__.cas = coder['cas']
+      end
+
+      self.class.define_attribute_methods
+
+      yield self if block_given?
+
+      run_callbacks :find
+      run_callbacks :initialize
+
+      self
+    end
+
+    # Returns true if this object hasn't been saved yet -- that is, a record
+    # for the object doesn't exist in the database yet; otherwise, returns false.
     def new_record?
       @__metadata__.cas.nil?
     end
+
     alias new? new_record?
 
-      # Returns true if this object has been destroyed, otherwise returns false.
+    # Returns true if this object has been destroyed, otherwise returns false.
     def destroyed?
       @destroyed
     end
 
-      # Returns true if the record is persisted, i.e. it's not a new record and it was
-      # not destroyed, otherwise returns false.
+    # Returns true if the record is persisted, i.e. it's not a new record and it was
+    # not destroyed, otherwise returns false.
     def persisted?
       !new_record? && !destroyed?
     end
+
     alias exists? persisted?
 
       # Saves the model.
@@ -87,23 +135,23 @@ module CouchbaseOrm
       create_or_update(**options, &block)
     end
 
-      # Saves the model.
-      #
-      # If the model is new, a record gets created in the database, otherwise
-      # the existing record gets updated.
-      #
-      # By default, #save! always runs validations. If any of them fail
-      # CouchbaseOrm::Error::RecordInvalid gets raised, and the record won't be saved.
+    # Saves the model.
+    #
+    # If the model is new, a record gets created in the database, otherwise
+    # the existing record gets updated.
+    #
+    # By default, #save! always runs validations. If any of them fail
+    # CouchbaseOrm::Error::RecordInvalid gets raised, and the record won't be saved.
     def save!(**options)
       self.class.fail_validate!(self) unless self.save(**options)
       self
     end
 
-      # Deletes the record in the database and freezes this instance to
-      # reflect that no changes should be made (since they can't be
-      # persisted). Returns the frozen instance.
-      #
-      # The record is simply removed, no callbacks are executed.
+    # Deletes the record in the database and freezes this instance to
+    # reflect that no changes should be made (since they can't be
+    # persisted). Returns the frozen instance.
+    #
+    # The record is simply removed, no callbacks are executed.
     def delete(**options)
       options[:cas] = @__metadata__.cas if options.delete(:with_cas)
       CouchbaseOrm.logger.debug "Data - Delete #{self.id}"
@@ -118,10 +166,10 @@ module CouchbaseOrm
 
     alias remove delete
 
-      # Deletes the record in the database and freezes this instance to reflect
-      # that no changes should be made (since they can't be persisted).
-      #
-      # There's a series of callbacks associated with #destroy.
+    # Deletes the record in the database and freezes this instance to reflect
+    # that no changes should be made (since they can't be persisted).
+    #
+    # There's a series of callbacks associated with #destroy.
     def destroy(**options)
       return self if destroyed?
       raise 'model not persisted' unless persisted?
@@ -140,13 +188,14 @@ module CouchbaseOrm
         freeze
       end
     end
+
     alias destroy! destroy
 
-      # Updates a single attribute and saves the record.
-      # This is especially useful for boolean flags on existing records. Also note that
-      #
-      # * Validation is skipped.
-      # * \Callbacks are invoked.
+    # Updates a single attribute and saves the record.
+    # This is especially useful for boolean flags on existing records. Also note that
+    #
+    # * Validation is skipped.
+    # * \Callbacks are invoked.
     def update_attribute(name, value)
       public_send(:"#{name}=", value)
       changed? ? save(validate: false) : true
@@ -156,57 +205,59 @@ module CouchbaseOrm
       super(hash.except('type'))
     end
 
-      # Updates the attributes of the model from the passed-in hash and saves the
-      # record. If the object is invalid, the saving will fail and false will be returned.
+    # Updates the attributes of the model from the passed-in hash and saves the
+    # record. If the object is invalid, the saving will fail and false will be returned.
     def update(hash)
       assign_attributes(hash)
       save
     end
+
     alias update_attributes update
 
-      # Updates its receiver just like #update but calls #save! instead
-      # of +save+, so an exception is raised if the record is invalid and saving will fail.
+    # Updates its receiver just like #update but calls #save! instead
+    # of +save+, so an exception is raised if the record is invalid and saving will fail.
     def update!(hash)
       assign_attributes(hash) # Assign attributes is provided by ActiveModel::AttributeAssignment
       save!
     end
+
     alias update_attributes! update!
 
-      # Updates the record without validating or running callbacks.
-      # Updates only the attributes that are passed in as parameters
-      # except if there is more than 16 attributes, in which case
-      # the whole record is saved.
+    # Updates the record without validating or running callbacks.
+    # Updates only the attributes that are passed in as parameters
+    # except if there is more than 16 attributes, in which case
+    # the whole record is saved.
     def update_columns(with_cas: false, **hash)
       raise 'unable to update columns, model not persisted' unless id
 
       assign_attributes(hash)
 
-      options = {extended: true}
+      options = { extended: true }
       options[:cas] = @__metadata__.cas if with_cas
 
-        # There is a limit of 16 subdoc operations per request
+      # There is a limit of 16 subdoc operations per request
       resp = if hash.length <= 16
                self.class.collection.mutate_in(
                  id,
                  hash.map { |k, v| Couchbase::MutateInSpec.replace(k.to_s, v) }
                )
              else
-          # Fallback to writing the whole document
+               # Fallback to writing the whole document
                CouchbaseOrm.logger.debug { "Data - Replace #{id} #{attributes.to_s.truncate(200)}" }
                self.class.collection.replace(id, attributes.except('id').merge(type: self.class.design_document),
                                              **options)
              end
 
-        # Ensure the model is up to date
+      # Ensure the model is up to date
       @__metadata__.cas = resp.cas
 
       changes_applied
       self
     end
 
-      # Reloads the record from the database.
-      #
-      # This method finds record by its key and modifies the receiver in-place:
+    # Reloads the record from the database.
+    #
+    # This method finds record by its key and modifies the receiver in-place:
     def reload
       raise 'unable to reload, model not persisted' unless id
 
@@ -220,7 +271,7 @@ module CouchbaseOrm
       self
     end
 
-      # Updates the TTL of the document
+    # Updates the TTL of the document
     def touch(**options)
       CouchbaseOrm.logger.debug "Data - Touch #{id}"
       _res = self.class.collection.touch(id, async: false, **options)
@@ -234,8 +285,8 @@ module CouchbaseOrm
 
     def _update_record(*)
       return true unless changed? || self.class.attribute_types.any? { |_, type|
-                           type.is_a?(CouchbaseOrm::Types::Nested) || type.is_a?(CouchbaseOrm::Types::Array)
-                         }
+        type.is_a?(CouchbaseOrm::Types::Nested) || type.is_a?(CouchbaseOrm::Types::Array)
+      }
 
       run_callbacks :update do
         run_callbacks :save do
@@ -245,7 +296,7 @@ module CouchbaseOrm
           resp = self.class.collection.replace(id,
                                                serialized_attributes.except('id').merge(type: self.class.design_document), Couchbase::Options::Replace.new(**options))
 
-            # Ensure the model is up to date
+          # Ensure the model is up to date
           @__metadata__.cas = resp.cas
 
           changes_applied
@@ -262,7 +313,7 @@ module CouchbaseOrm
           resp = self.class.collection.upsert(self.id,
                                               serialized_attributes.except('id').merge(type: self.class.design_document), Couchbase::Options::Upsert.new)
 
-            # Ensure the model is up to date
+          # Ensure the model is up to date
           @__metadata__.cas = resp.cas
 
           changes_applied
@@ -272,3 +323,4 @@ module CouchbaseOrm
     end
   end
 end
+# rubocop:enable Metrics/ModuleLength
